5b5565b359771820bc408f9d0730132d3fb8bee4
motdotla <mot@mot.la>
2017-02-07 13:51:30 -0800

    Change the type of field depending on the control

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.mobile
-//= require_tree .
 
 
---
+++ app/helpers/controls_helper.rb
@@ -0,0 +1,15 @@
+module ControlsHelper
+  def control_field(f, control)
+    field_name = "control_#{control.id}".to_sym
+
+    case control.control_type
+    when 'slider'
+      f.range_field field_name, min: 0, max: 100
+    when 'select'
+      f.select field_name, ['one', 'two']
+    when 'button'
+      f.button "IMPLEMENT"
+    else
+    end
+  end
+end
