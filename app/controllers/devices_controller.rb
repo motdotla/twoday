fd94bd553c86be2ab3776e21ebc53e1e035c9ae0
motdotla <mot@mot.la>
2017-02-07 10:20:27 -0800

    Updating device basics from mobile front-end

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,4 +2,24 @@ class DevicesController < ApplicationController
   def index
     @devices = Device.all
   end
+
+  def edit
+    @device = Device.find(params[:id])
+  end
+
+  def update
+    @device = Device.find(params[:id])
+
+    if @device.update(device_params)
+      redirect_to devices_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def device_params
+    params.require(:device).permit(:name, :ip_address)
+  end
 end
